f4546bf9f78cdf3b63dbfe4c5a73c455890a84e2
Oleksandr Panichuk <oleksandr.panichuk.dev@gmail.com>
2025-06-30 00:07:11 +0300

    Navbar section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import gsap from "gsap";
-import {SplitText, ScrollTrigger} from "gsap/all";
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from 'gsap/all';
+import { Navbar } from './components';
 
-gsap.registerPlugin(ScrollTrigger, SplitText)
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export const App = () => {
- return (
-  <div>
-
-  </div>
-  );
- }
+    return (
+        <main>
+            <Navbar />
+        </main>
+    );
+};
