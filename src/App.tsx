144543fd71e834462fc1b01c0fb561dec45f1e3a
Oleksandr Panichuk <oleksandr.panichuk.dev@gmail.com>
2025-06-29 23:47:15 +0300

    setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import gsap from "gsap";
+import {SplitText, ScrollTrigger} from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText)
+
+export const App = () => {
+ return (
+  <div>
+
+  </div>
+  );
+ }
